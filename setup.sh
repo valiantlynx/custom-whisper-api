933ceea97e3a5072f76d8b03d8f170d3076d6dd0
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-02-03 13:31:45 +0100

    rename

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # run using docker
-docker build -t depth-first-search-image .
-docker run --name depth-first-search-container -d -p 8000:8000 -v $(pwd):/code depth-first-search-image
+docker build -t custom-whisper-api-image .
+docker run --name custom-whisper-api-container -d -p 8000:8000 -v $(pwd):/code custom-whisper-api-image
 
 #connect to turborepo
 git subtree add --prefix=apps/custom-whisper-api https://github.com/valiantlynx/custom-whisper-api.git main --squash
