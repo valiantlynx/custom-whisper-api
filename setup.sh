ad155830c9bae4f0386b8dea3b085ba75e394ac3
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2024-02-03 13:25:31 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ docker build -t depth-first-search-image .
 docker run --name depth-first-search-container -d -p 8000:8000 -v $(pwd):/code depth-first-search-image
 
 #connect to turborepo
-git subtree add --prefix=apps/custom-whisper-api https://github.com/valiantlynx/custom-whisper-api.git master --squash
-git subtree pull --prefix=apps/custom-whisper-api https://github.com/valiantlynx/custom-whisper-api.git master --squash
-git subtree push --prefix=apps/custom-whisper-api https://github.com/valiantlynx/custom-whisper-api.git master
+git subtree add --prefix=apps/custom-whisper-api https://github.com/valiantlynx/custom-whisper-api.git main --squash
+git subtree pull --prefix=apps/custom-whisper-api https://github.com/valiantlynx/custom-whisper-api.git main --squash
+git subtree push --prefix=apps/custom-whisper-api https://github.com/valiantlynx/custom-whisper-api.git main
